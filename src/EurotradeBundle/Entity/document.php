4835fd7cf307d122a04f26e4e20f0bd4b38cb0d3
bruno-bouvet <bouvet.bruno@gmail.com>
2016-12-23 19:51:21 +0100

    creation tables et relations manque Fosuser

---
+++ src/EurotradeBundle/Entity/document.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace EurotradeBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * document
+ */
+class document
+{
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $format;
+
+    /**
+     * @var string
+     */
+    private $file;
+
+    /**
+     * @var string
+     */
+    private $url;
+
+    /**
+     * @var \EurotradeBundle\Entity\blog
+     */
+    private $blog;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return document
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set format
+     *
+     * @param string $format
+     *
+     * @return document
+     */
+    public function setFormat($format)
+    {
+        $this->format = $format;
+
+        return $this;
+    }
+
+    /**
+     * Get format
+     *
+     * @return string
+     */
+    public function getFormat()
+    {
+        return $this->format;
+    }
+
+    /**
+     * Set file
+     *
+     * @param string $file
+     *
+     * @return document
+     */
+    public function setFile($file)
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+
+    /**
+     * Get file
+     *
+     * @return string
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     *
+     * @return document
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set blog
+     *
+     * @param \EurotradeBundle\Entity\blog $blog
+     *
+     * @return document
+     */
+    public function setBlog(\EurotradeBundle\Entity\blog $blog = null)
+    {
+        $this->blog = $blog;
+
+        return $this;
+    }
+
+    /**
+     * Get blog
+     *
+     * @return \EurotradeBundle\Entity\blog
+     */
+    public function getBlog()
+    {
+        return $this->blog;
+    }
+}
