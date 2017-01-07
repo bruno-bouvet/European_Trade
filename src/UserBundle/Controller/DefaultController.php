fc5167c3e8e4f876a3b3f00784dd8182bd147476
bruno-bouvet <bouvet.bruno@gmail.com>
2017-01-07 11:35:53 +0100

    a jeter si tout va bien

--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -7,8 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-//    public function indexAction()
-//    {
-//        return $this->render('UserBundle:Default:index.html.twig');
-//    }
+    public function indexAction()
+    {
+        return $this->render('UserBundle:Default:index.html.twig');
+    }
 }
