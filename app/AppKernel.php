54e9c5e90d0c5619114a5b1e92215ff14f110b84
bruno-bouvet <bouvet.bruno@gmail.com>
2017-08-07 12:55:37 +0200

    installed mopa and assetic

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new EurotradeBundle\EurotradeBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
